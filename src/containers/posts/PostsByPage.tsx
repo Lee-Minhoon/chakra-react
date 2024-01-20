d8809bae9190a3a37f35711b976064a9b2759bb1
Lee Minhoon <hackids@naver.com>
2024-01-21 04:04:12 +0900

    fixed page query loading

--- src/containers/posts/PostsByPage.tsx
+++ src/containers/posts/PostsByPage.tsx
@@ -9,7 +9,7 @@ import { useRouter } from "next/router";
 const PostsByPage = () => {
   const router = useRouter();
   const { page, limit, sort, order, onPagination } = usePagination();
-  const { data: postsByPage, isFetching: postsIsLoading } = useGetPostsByPage({
+  const { data: postsByPage, isLoading: postsIsLoading } = useGetPostsByPage({
     page,
     limit,
     sort,
