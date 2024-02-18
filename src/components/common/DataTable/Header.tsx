54ca384457fe65021b360b45c721a5bb8b4a28a1
Lee Minhoon <hackids@naver.com>
2024-02-19 01:00:47 +0900

    update data table

--- src/components/common/DataTable/Header.tsx
+++ src/components/common/DataTable/Header.tsx
@@ -18,45 +18,49 @@ const DataTableHeader = <T,>({ table }: DataTableHeaderProps<T>) => {
     <Thead>
       {table.getHeaderGroups().map((headerGroup) => (
         <Tr key={headerGroup.id}>
-          {headerGroup.headers.map((header) => (
-            <Th key={header.id} px={4}>
-              <Flex align={"center"} gap={2}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                {header.column.columnDef.meta?.sortable && (
-                  <IconButton
-                    aria-label="sort"
-                    size={"xs"}
-                    onClick={() =>
-                      onPagination({
-                        sort: header.id,
-                        order:
-                          sort === header.id
+          {headerGroup.headers.map((header) => {
+            const isSorted = sort === header.id;
+
+            return (
+              <Th key={header.id} px={4}>
+                <Flex align={"center"} gap={2}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                  {header.column.columnDef.meta?.sortable && (
+                    <IconButton
+                      aria-label={"sort"}
+                      variant={isSorted ? "solid" : "outline"}
+                      size={"xs"}
+                      onClick={() =>
+                        onPagination({
+                          sort: header.id,
+                          order: isSorted
                             ? order === "desc"
                               ? "asc"
                               : "desc"
                             : "desc",
-                      })
-                    }
-                  >
-                    <Icon
-                      as={
-                        sort === header.id
-                          ? order === "asc"
-                            ? TiArrowSortedUp
-                            : TiArrowSortedDown
-                          : TiArrowUnsorted
+                        })
                       }
-                    />
-                  </IconButton>
-                )}
-              </Flex>
-            </Th>
-          ))}
+                    >
+                      <Icon
+                        as={
+                          isSorted
+                            ? order === "asc"
+                              ? TiArrowSortedUp
+                              : TiArrowSortedDown
+                            : TiArrowUnsorted
+                        }
+                      />
+                    </IconButton>
+                  )}
+                </Flex>
+              </Th>
+            );
+          })}
         </Tr>
       ))}
     </Thead>
