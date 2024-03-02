a35fe5827d139a46b832c0cd595f04214f3f7fc1
Lee Minhoon <hackids@naver.com>
2024-03-03 07:31:58 +0900

    fix useFormatDate

--- src/hooks/useFormatDate.ts
+++ src/hooks/useFormatDate.ts
@@ -1,4 +1,5 @@
 import { fillZero } from "@/utils";
+import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 
 const MINUTE = 60 * 1000;
@@ -10,33 +11,36 @@ const YEAR = 365 * DAY;
 const useFormatDate = () => {
   const { t } = useTranslation();
 
-  return (value?: string) => {
-    if (!value) return "";
+  return useCallback(
+    (value?: string) => {
+      if (!value) return "";
 
-    const now = new Date();
-    const date = new Date(value);
-    const diff = now.getTime() - date.getTime();
+      const now = new Date();
+      const date = new Date(value);
+      const diff = now.getTime() - date.getTime();
 
-    if (diff < MINUTE) {
-      return t("Just now");
-    } else if (diff < HOUR) {
-      return `${Math.floor(diff / MINUTE)} ${t("minutes ago")}`;
-    } else if (diff < DAY) {
-      return `${Math.floor(diff / HOUR)} ${t("hours ago")}`;
-    } else if (diff < WEEK) {
-      return `${Math.floor(diff / DAY)} ${t("days ago")}`;
-    }
+      if (diff < MINUTE) {
+        return t("Just now");
+      } else if (diff < HOUR) {
+        return `${Math.floor(diff / MINUTE)} ${t("minutes ago")}`;
+      } else if (diff < DAY) {
+        return `${Math.floor(diff / HOUR)} ${t("hours ago")}`;
+      } else if (diff < WEEK) {
+        return `${Math.floor(diff / DAY)} ${t("days ago")}`;
+      }
 
-    const year = date.getFullYear();
-    const month = fillZero(date.getMonth() + 1, 2);
-    const day = fillZero(date.getDate(), 2);
+      const year = date.getFullYear();
+      const month = fillZero(date.getMonth() + 1, 2);
+      const day = fillZero(date.getDate(), 2);
 
-    if (diff < YEAR) {
-      return `${month}-${day}`;
-    } else {
-      return `${year}-${month}-${day}`;
-    }
-  };
+      if (diff < YEAR) {
+        return `${month}-${day}`;
+      } else {
+        return `${year}-${month}-${day}`;
+      }
+    },
+    [t]
+  );
 };
 
 export default useFormatDate;
