c94b5b6f54512cec886de2092ba44afc6e8ce23e
Lee Minhoon <hackids@naver.com>
2024-03-03 04:26:37 +0900

    update chakra theme

--- src/components/providers/ChakraProvider/theme.ts
+++ src/components/providers/ChakraProvider/theme.ts
@@ -12,6 +12,13 @@ const theme = extendTheme(
     },
     colors: {
       primary: baseTheme.colors.teal,
+      bg: "var(--chakra-colors-chakra-body-bg)",
+      subtleBg: "var(--chakra-colors-chakra-subtle-bg)",
+      text: "var(--chakra-colors-chakra-body-text)",
+      subtleText: "var(--chakra-colors-chakra-subtle-text)",
+      inverseText: "var(--chakra-colors-chakra-inverse-text)",
+      placeholder: "var(--chakra-colors-chakra-placeholder-color)",
+      border: "var(--chakra-colors-chakra-border-color)",
     },
     components: {
       Input: {
