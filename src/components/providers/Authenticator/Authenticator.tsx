0db05147b0e404402aaaa0e85cd236bca910ff4a
Lee Minhoon <hackids@naver.com>
2024-03-03 11:35:05 +0900

    update unauthorized warning modal

--- src/components/providers/Authenticator/Authenticator.tsx
+++ src/components/providers/Authenticator/Authenticator.tsx
@@ -2,13 +2,15 @@ import { useGetMe } from "@/apis/auth";
 import { PageRoutes, isExistPage, whiteList } from "@/constants";
 import { useSafePush } from "@/hooks";
 import { useModalStore } from "@/stores";
+import { toUrl } from "@/utils";
+import { Button, Flex, Text } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 const Authenticator = () => {
   const { router, push } = useSafePush();
   const { data, isFetching } = useGetMe();
-  const { openAlert } = useModalStore(["openAlert"]);
+  const { openAlert, closeAlert } = useModalStore(["openAlert", "closeAlert"]);
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -19,16 +21,40 @@ const Authenticator = () => {
     )
       return;
     if (!data) {
-      openAlert({
-        title: t("Unauthorized"),
-        content: t("You are not authorized to access this page"),
-      });
       push({
         pathname: PageRoutes.Signin,
         query: { redirect: router.asPath },
+      })?.then(() => {
+        openAlert({
+          title: t("Unauthorized"),
+          content: (
+            <Flex direction={"column"} align={"center"} gap={"4"}>
+              <Text>{t("You are not authorized to access this page")}</Text>
+              <Flex gap={"4"}>
+                <Button
+                  variant={"outline"}
+                  onClick={() => {
+                    router.push(toUrl(PageRoutes.Home));
+                    closeAlert();
+                  }}
+                >
+                  {t("Go Home")}
+                </Button>
+                <Button
+                  onClick={() => {
+                    router.back();
+                    closeAlert();
+                  }}
+                >
+                  {t("Go Back")}
+                </Button>
+              </Flex>
+            </Flex>
+          ),
+        });
       });
     }
-  }, [data, isFetching, openAlert, push, router.asPath, router.pathname, t]);
+  }, [closeAlert, data, isFetching, openAlert, push, router, t]);
 
   return <></>;
 };
--- src/hooks/useSafePush.tsx
+++ src/hooks/useSafePush.tsx
@@ -1,7 +1,8 @@
 import { useGetMe } from "@/apis";
 import { PageRoutes, whiteList } from "@/constants";
 import { useModalStore } from "@/stores";
-import { NextURL } from "@/utils";
+import { NextURL, toUrl } from "@/utils";
+import { Button, Flex, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
@@ -12,7 +13,7 @@ const useSafePush = () => {
   const router = useRouter();
   const { data, isFetching } = useGetMe();
   const isChanging = useRef(false);
-  const { openAlert } = useModalStore(["openAlert"]);
+  const { openAlert, closeAlert } = useModalStore(["openAlert", "closeAlert"]);
   const { t } = useTranslation();
 
   const push = useCallback(
@@ -29,19 +30,44 @@ const useSafePush = () => {
       // 만약 라우팅이 허용되지 않은 페이지로 이동하려고 한다면
       // If the user tries to navigate to a page that is not allowed
       if (!whiteList.includes(nextURL?.pathname ?? router.pathname) && !data) {
-        openAlert({
-          title: t("Unauthorized"),
-          content: t("You are not authorized to access this page"),
-        });
-        router.push({
-          pathname: PageRoutes.Signin,
-          query: { redirect: nextURL.toString() },
-        });
-        return;
+        return router
+          .push({
+            pathname: PageRoutes.Signin,
+            query: { redirect: router.asPath },
+          })
+          .then(() => {
+            openAlert({
+              title: t("Unauthorized"),
+              content: (
+                <Flex direction={"column"} align={"center"} gap={"4"}>
+                  <Text>{t("You are not authorized to access this page")}</Text>
+                  <Flex gap={"4"}>
+                    <Button
+                      variant={"outline"}
+                      onClick={() => {
+                        router.push(toUrl(PageRoutes.Home));
+                        closeAlert();
+                      }}
+                    >
+                      {t("Go Home")}
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        router.back();
+                        closeAlert();
+                      }}
+                    >
+                      {t("Go Back")}
+                    </Button>
+                  </Flex>
+                </Flex>
+              ),
+            });
+          });
       }
-      router.push(...params);
+      return router.push(...params);
     },
-    [data, isFetching, openAlert, router, t]
+    [closeAlert, data, isFetching, openAlert, router, t]
   );
 
   const handleRouteChange = useCallback(() => {
