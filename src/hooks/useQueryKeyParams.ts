fc5862229f36bae40880767e3a5d01949661748e
Lee Minhoon <hackids@naver.com>
2024-03-08 01:36:54 +0900

    fix query invalidation bug

--- src/hooks/useQueryKeyParams.ts
+++ src/hooks/useQueryKeyParams.ts
@@ -1,28 +1,23 @@
 import { QueryKey } from "@/apis";
 import { useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
 
 const useQueryKeyParams = (queryKey: QueryKey[0]) => {
   const queryClient = useQueryClient();
   const router = useRouter();
 
-  return useMemo(
-    () =>
-      queryClient
-        .getQueryCache()
-        .findAll([queryKey])
-        .find((query) => {
-          if (!query.queryKey[1]) return false;
-          return Object.entries(query.queryKey[1]).reduce(
-            (isMatch, [key, value]) => {
-              return isMatch && router.query[key] === value.toString();
-            },
-            true
-          );
-        })?.queryKey[1],
-    [queryClient, queryKey, router.query]
-  ) as QueryKey[1];
+  return queryClient
+    .getQueryCache()
+    .findAll([queryKey])
+    .find((query) => {
+      if (!query.queryKey[1]) return false;
+      return Object.entries(query.queryKey[1]).reduce(
+        (isMatch, [key, value]) => {
+          return isMatch && router.query[key] === value.toString();
+        },
+        true
+      );
+    })?.queryKey[1] as QueryKey[1];
 };
 
 export default useQueryKeyParams;
